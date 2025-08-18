80a7fd1795cc5234f6277e2768b4575d25ded073
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-08-18 14:16:49 -0300

    Renomiei o app de autenticação para evitar conflito

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('_shortener.urls', namespace='shortener')),
-    path('', include('auth.urls', namespace='auth')),
+    path('', include('authentication.urls', namespace='authentication')),
 ]
