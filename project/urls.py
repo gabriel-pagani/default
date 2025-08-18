0eacb789a15156db236ee47694f5539cbe7d025d
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-08-18 13:18:29 -0300

    Iniciei um app para gerenciar a autenticação dos usuários

--- project/urls.py
+++ project/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('_shortener.urls', namespace='shortener')),
+    path('', include('auth.urls', namespace='auth')),
 ]
